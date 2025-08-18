70498bc792cfbb3d88c45bfe31debe21866c62d5
asmaembr <asmae.moubr@gmail.com>
2025-08-19 00:41:17 +0100

    fiiixing unit tests and adding style

--- src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5078cf8365456597088ee4042c5fab985d9927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986ed6bdc4dcd1869dcdcafdb2efdaa464add656")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
