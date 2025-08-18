0a62309b37e720dc73951cf6fb2ded3b52ede197
asmaembr <asmae.moubr@gmail.com>
2025-08-18 23:30:13 +0100

    dependency injection changes / backend api tested and runned successfully

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using MediatR; 
+using System.Reflection;
+using FluentValidation;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
